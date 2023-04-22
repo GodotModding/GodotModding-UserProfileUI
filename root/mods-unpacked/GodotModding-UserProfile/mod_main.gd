de5ae9505355d19fc087f79983255c0184ea1b85
Kai Roth <kai@kana.jetzt>
2023-04-23 01:04:58 +0200

    fix: :bug: broken dependencies
    renamed everything from `KANA` to `GodotModding`

--- root/mods-unpacked/GodotModding-UserProfile/mod_main.gd
+++ root/mods-unpacked/GodotModding-UserProfile/mod_main.gd
@@ -1,8 +1,8 @@
 extends Node
 
 
-var KANA_User_Profile_MOD_DIR := "KANA-UserProfile"
-var KANA_User_Profile_LOG_NAME := "KANA-UserProfile"
+var GodotModding_User_Profile_MOD_DIR := "GodotModding-UserProfile"
+var GodotModding_User_Profile_LOG_NAME := "GodotModding-UserProfile"
 
 var mod_dir_path := ""
 var extensions_dir_path := ""
@@ -10,8 +10,8 @@ var translations_dir_path := ""
 
 
 func _init(modLoader = ModLoader) -> void:
-	ModLoaderUtils.log_info("Init", KANA_User_Profile_LOG_NAME)
-	mod_dir_path = modLoader.UNPACKED_DIR.plus_file(KANA_User_Profile_MOD_DIR)
+	ModLoaderUtils.log_info("Init", GodotModding_User_Profile_LOG_NAME)
+	mod_dir_path = modLoader.UNPACKED_DIR.plus_file(GodotModding_User_Profile_MOD_DIR)
 
 	# Add extensions
 	install_script_extensions(modLoader)
@@ -29,5 +29,5 @@ func add_translations(modLoader) -> void:
 
 
 func _ready():
-	var user_profile_dialog = load("res://mods-unpacked/KANA-UserProfile/content/UserProfiles.tscn").instance()
+	var user_profile_dialog = load("res://mods-unpacked/GodotModding-UserProfile/content/UserProfiles.tscn").instance()
 	get_tree().root.call_deferred("add_child", user_profile_dialog)
